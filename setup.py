534bedd650c30bc2be41335a0ecbbf3322064701
Frank Sachsenheim <funkyfuture@riseup.net>
2019-03-18 21:01:13 +0100

    Adds a setup.py to allow installation on RTD

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# for now this file exists solely to run on ReadTheDocs
+
+from setuptools import setup
+
+
+VERSION = '0.1a1'
+
+setup(
+    name='lxml-domesque',
+    version=VERSION,
+    packages=['lxml_domesque'],
+    install_requires=['lxml'],
+)
