8a93908c37634943978b27fd466fc3eb6559d064
Frank Sachsenheim <funkyfuture@riseup.net>
2022-08-11 23:40:14 +0200

    Removes a lot of legacy code

--- _delb/xpath/__init__.py
+++ _delb/xpath/__init__.py
@@ -58,20 +58,16 @@ custom functions.
 
 from __future__ import annotations
 
-from abc import ABC
-from collections import UserString
 from functools import lru_cache
 from typing import (
     TYPE_CHECKING,
     Collection,
-    Dict,
     Iterable,
     Iterator,
     List,
     Mapping,
     Optional,
     Sequence,
-    Set,
     Tuple,
 )
 
@@ -195,122 +191,7 @@ __all__ = (
 #  L E G A C Y  #
 
 
-""" This is not an XPath implementation. """
-
-# ordered by alphabet
-AXIS_NAMES = (
-    "ancestor",
-    "ancestor-or-self",
-    "attribute",
-    "child",
-    "descendant",
-    "descendant-or-self",
-    "following",
-    "following-sibling",
-    "namespace",
-    "parent",
-    "preceding",
-    "preceding-sibling",
-    "self",
-)
-# ordered by weight (ascending)
-BOOLEAN_OPERATORS = ("or", "and", "!=", "=", ">", ">=", "<", "<=")
-BOOLEAN_FUNCTIONS = ("contains", "not", "starts-with")
-
-
-def _in_parenthesis(expression: str) -> bool:
-    """
-    Determines whether an expression is surrounded by parenthesis in its
-    entirety.
-
-    >>> _in_parenthesis('foo and bar')
-    False
-
-    >>> _in_parenthesis('(foo)(bar)')
-    False
-
-    >>> _in_parenthesis('foo)') == _in_parenthesis('(foo') == False
-    True
-
-    >>> _in_parenthesis('((foo)(bar))')
-    True
-
-    """
-    if not expression.startswith("(") or not expression.endswith(")"):
-        return False
-    level = 1
-    for character in expression[1:-1]:
-        if character == "(":
-            level += 1
-        elif character == ")":
-            level -= 1
-        if level < 1:
-            return False
-    return True
-
-
-def _partition_terms(expression: str) -> List[str]:  # noqa: C901
-    """
-    Split up expression into partitions under consideration of quotes and
-    parentheses.
-
-    >>> _partition_terms('(@foo="1") and (@bar="2")')
-    ['@foo="1"', 'and', '@bar="2"']
-
-    >>> _partition_terms('@foo = "1"')
-    ['@foo', '=', '"1"']
-
-    >>> _partition_terms('((@foo="1") or (@bar="2")) and @baz!="3"')
-    ['(@foo="1") or (@bar="2")', 'and', '@baz!="3"']
-
-    >>> _partition_terms('@href and starts-with(@href,"https://")')
-    ['@href', 'and', 'starts-with(@href,"https://")']
-
-    >>> _partition_terms('@href and not(starts-with(@href,"https://"))')
-    ['@href', 'and', 'not(starts-with(@href,"https://"))']
-
-    >>> _partition_terms('not(@foo) and (@bar)')
-    ['not(@foo)', 'and', '@bar']
-
-    """
-    bracket_level = 0
-    function_args = False
-    current_term = ""
-    quote = ""
-    result = []
-
-    for character in expression:
-        if quote or bracket_level:
-            if character == quote:
-                quote = ""
-            elif character == "(":
-                bracket_level += 1
-            elif character == ")":
-                bracket_level -= 1
-                if not bracket_level and not function_args:
-                    continue
-                function_args = function_args and bool(bracket_level)
-            current_term += character
-        elif character in ("'", '"'):
-            quote = character
-            current_term += character
-        elif character == "(":
-            bracket_level += 1
-            if current_term in BOOLEAN_FUNCTIONS:
-                current_term += character
-                function_args = True
-        elif character == ")":
-            pass
-        elif character == " ":
-            result.append(current_term)
-            current_term = ""
-        else:
-            current_term += character
-
-    if current_term:
-        result.append(current_term)
-
-    return result
+""" This was neither an XPath implementation. """
 
 
 def _reduce_whitespace(expression: str) -> str:
@@ -388,7 +269,7 @@ def _split(expression: str, separator: str) -> Iterator[str]:
     yield part
 
 
-class LegacyXPathExpression(UserString):
+class LegacyXPathExpression:
     __slots__ = ("location_paths",)
 
     def __init__(self, expression: str):
@@ -396,53 +277,8 @@ class LegacyXPathExpression(UserString):
             LegacyLocationPath(x) for x in _split(_reduce_whitespace(expression), "|")
         ]
 
-    @property
-    def data(self):
+    def __str__(self):
         return " | ".join(str(x) for x in self.location_paths)
-
-    def is_unambiguously_locatable(self) -> bool:
-        """
-        Determine whether an xpath expression addresses exactly one possible node.
-
-        Examples::
-
-        >>> LegacyXPathExpression('./path1|./path2').is_unambiguously_locatable()
-        False
-
-        >>> LegacyXPathExpression('/path').is_unambiguously_locatable()
-        False
-
-        >>> LegacyXPathExpression(
-        './root/node/descendant::foo').is_unambiguously_locatable()
-        False
-
-        >>> LegacyXPathExpression('./node[@a="1" or @b="2"]').is_unambiguously_locatable()
-        False
-
-        >>> LegacyXPathExpression('./root/node[@a="1"][@b="2"]').is_unambiguously_locatable()
-        True
-
-        """  # noqa: E501
-        if len(self.location_paths) != 1:
-            return False
-
-        steps = self.location_paths[0].location_steps
-
-        if steps[0].axis != "self":
-            return False
-
-        for step in steps[1:]:
-            if step.axis != "child":
-                return False
-
-            predicates = step.predicates
-            if predicates is None:
-                continue
-
-            if not predicates._can_describe_attributes:
-                return False
-
-        return True
 
 
 class LegacyLocationPath:
@@ -464,7 +300,7 @@ class LegacyLocationStep:
     def __init__(self, expression: str):
         self.axis: str
         self.node_test: LegacyNodeTest
-        self.predicates: Optional[LegacyPredicateExpression] = None
+        self.predicates = ""
 
         if expression == "":
             self.axis = "descendant-or-self"
@@ -479,10 +315,7 @@ class LegacyLocationStep:
             if "[" in expression:
                 expression, predicates_part = expression.split("[", maxsplit=1)
                 assert predicates_part[-1] == "]", predicates_part
-                self.predicates = LegacyPredicateExpression.parse("[" + predicates_part)
-
-            if expression.startswith("@"):
-                expression = "attribute::" + expression[1:]
+                self.predicates = "[" + predicates_part
 
             if "::" not in expression:
                 self.axis = "child"
@@ -491,17 +324,8 @@ class LegacyLocationStep:
                 self.axis, node_test_part = expression.split("::")
                 self.node_test = LegacyNodeTest(node_test_part)
 
-        if self.axis not in AXIS_NAMES:
-            raise ValueError(f"`{self.axis}` is not a valid axis name.")
-
     def __str__(self):
-        result = self.axis + "::" + str(self.node_test)
-        if self.predicates is not None:
-            result += "[" + str(self.predicates) + "]"
-        return result
-
-    def _derive_attributes(self):
-        return self.predicates._derive_attributes()
+        return self.axis + "::" + self.node_test.data + self.predicates
 
 
 class LegacyNodeTest:
@@ -514,239 +338,3 @@ class LegacyNodeTest:
             self.type = "type_test"
         else:
             self.type = "name_test"
-
-    def __str__(self):
-        return self.data
-
-
-class LegacyPredicateExpression(ABC):
-    def evaluate(self, node_set: Set["TagNode"]) -> Set["TagNode"]:
-        # shall not be implemented
-        raise NotImplementedError
-
-    @staticmethod
-    @lru_cache(64)
-    def parse(expression: str) -> "LegacyPredicateExpression":
-        """
-                Parse string expression into ``PredicateExpression`` subclass instance.
-
-        import _delb.xpath.parser        >>> isinstance(_delb.xpath.parser.parse('[(@foo)]'),
-                LegacyAttributePredicate)
-                True
-
-        import _delb.xpath.parser        >>> isinstance(_delb.xpath.parser.parse('[@a="1" or
-        @b="2"]'),
-                BooleanPredicate) # noqa: E501
-                True
-
-        import _delb.xpath.parser        >>> isinstance(_delb.xpath.parser.parse('[1]'),
-        LegacyIndexPredicate)
-                True
-
-        import _delb.xpath.parser        >>> str(_delb.xpath.parser.parse(
-                ...     '[@href and starts-with(@href, "https://")]'))
-                '(@href and starts-with(@href,"https://"))'
-
-        """
-        if expression.startswith("["):
-            expression = expression[1:-1]
-        expressions = tuple(_split(expression, "]["))
-
-        if len(expressions) > 1:
-            parsed_expressions = [
-                LegacyPredicateExpression.parse(e) for e in expressions[::-1]
-            ]
-
-            while len(parsed_expressions) > 1:
-                parsed_expressions.append(
-                    LegacyBooleanPredicate(
-                        "and", parsed_expressions.pop(), parsed_expressions.pop()
-                    ),
-                )
-            return parsed_expressions[0]
-
-        while _in_parenthesis(expression):
-            expression = expression[1:-1]
-
-        partitions = _partition_terms(expression)
-
-        if len(partitions) == 1:
-            if expression.startswith("@") or expression.startswith("attribute::"):
-                return LegacyAttributePredicate.parse(expression)
-            elif expression.isdigit():
-                return LegacyIndexPredicate(expression)
-            elif any(expression.startswith(f) for f in BOOLEAN_FUNCTIONS):
-                return LegacyFunctionExpression(expression)
-            elif expression.startswith('"') or expression.endswith("'"):
-                return LegacyLiteral(expression)
-            else:
-                return LegacyUnsupportedPredicate(expression)
-
-        else:
-            return LegacyBooleanPredicate.from_partitions(partitions)
-
-    @property
-    def _can_describe_attributes(self) -> bool:
-        return False
-
-    def _derive_attributes(self) -> Dict[str, str]:
-        raise RuntimeError
-
-
-class LegacyAttributePredicate(LegacyPredicateExpression):
-    __slots__ = ("name", "value")
-
-    def __init__(self, name: str, value: Optional[str]):
-        self.name = name
-        self.value = value
-
-    @staticmethod
-    # related: https://github.com/python/mypy/issues/5107
-    def parse(expression: str) -> "LegacyAttributePredicate":  # type: ignore
-        """
-                Parse an xpath predicate string expression into an ``AttributePredicate``
-                instance.
-
-        import _delb.xpath.parser        >>> isinstance(
-                ...     _delb.xpath.parser.parse('@type="translation"'),
-                LegacyAttributePredicate
-                ... )
-                True
-
-        """  # noqa: E501
-        if expression.startswith("attribute::"):
-            raise NotImplementedError
-        assert expression.startswith("@")
-
-        parts = tuple(_split(expression[1:], "="))
-        if len(parts) == 1:
-            return LegacyAttributePredicate(parts[0], None)
-        elif len(parts) == 2:
-            return LegacyAttributePredicate(parts[0], parts[1][1:-1])
-        else:
-            raise ValueError
-
-    def __str__(self):
-        value = self.value
-        if value is None:
-            return f"@{self.name}"
-        quote = "'" if '"' in value else '"'
-        return f"@{self.name}={quote}{self.value}{quote}"
-
-    @property
-    def _can_describe_attributes(self) -> bool:
-        return self.value is not None
-
-    def _derive_attributes(self) -> Dict[str, str]:
-        assert self.value is not None
-        return {self.name: self.value}
-
-
-class LegacyBooleanPredicate(LegacyPredicateExpression):
-    __slots__ = ("operator", "left_operand", "right_operand")
-
-    def __init__(
-        self,
-        operator: str,
-        left_operand: LegacyPredicateExpression,
-        right_operand: LegacyPredicateExpression,
-    ):
-        self.operator = operator
-        self.left_operand = left_operand
-        self.right_operand = right_operand
-
-    def __str__(self):
-        sep = " " if self.operator in ("and", "or") else ""
-        return f"({self.left_operand}{sep}{self.operator}{sep}{self.right_operand})"
-
-    @staticmethod
-    def from_partitions(partitions: List[str]) -> LegacyPredicateExpression:
-        """
-        Instantiates a boolean predicate from a list of string expressions. If the
-        expression list passed contains only 1 element, an attribute or index predicate
-        object gets instantiated.
-
-        >>> bp = LegacyBooleanPredicate.from_partitions(['@foo="1"', 'and', '@bar="2"'])
-        >>> bp.operator
-        'and'
-
-        >>> isinstance(bp.left_operand, LegacyAttributePredicate)
-        True
-
-        """
-        if len(partitions) == 1:
-            return LegacyPredicateExpression.parse(partitions[0])
-
-        for operator in BOOLEAN_OPERATORS:
-            for i, token in tuple(enumerate(partitions))[-2::-2]:
-                if token == operator:
-                    return LegacyBooleanPredicate(
-                        operator,
-                        LegacyBooleanPredicate.from_partitions(partitions[:i]),
-                        LegacyBooleanPredicate.from_partitions(partitions[i + 1 :]),
-                    )
-
-        raise RuntimeError
-
-    @property
-    def _can_describe_attributes(self) -> bool:
-        return (
-            self.operator == "and"
-            and self.left_operand._can_describe_attributes
-            and self.right_operand._can_describe_attributes
-        )
-
-    def _derive_attributes(self) -> Dict[str, str]:
-        if self.operator != "and":
-            raise RuntimeError
-
-        return {
-            **self.left_operand._derive_attributes(),
-            **self.right_operand._derive_attributes(),
-        }
-
-
-class LegacyFunctionExpression(LegacyPredicateExpression):
-    __slots__ = ("arguments", "name")
-
-    def __init__(self, expression: str):
-        self.name, rest = expression.split("(", maxsplit=1)
-        self.arguments = tuple(
-            LegacyPredicateExpression.parse(e) for e in _split(rest[:-1], ",")
-        )
-
-    def __str__(self):
-        return f"{self.name}({','.join(str(a) for a in self.arguments)})"
-
-
-class LegacyIndexPredicate(LegacyPredicateExpression):
-    __slots__ = ("index",)
-
-    def __init__(self, index: str):
-        self.index = int(index)
-
-    def __str__(self):
-        return str(self.index)
-
-
-class LegacyLiteral(LegacyPredicateExpression):
-    __slots__ = ("value",)
-
-    def __init__(self, expression: str):
-        assert expression.endswith(expression[0])
-        self.value = expression[1:-1]
-
-    def __str__(self):
-        value = self.value
-        quote = "'" if '"' in value else '"'
-        return f"{quote}{value}{quote}"
-
-
-class LegacyUnsupportedPredicate(LegacyPredicateExpression):
-    __slots__ = ("expression",)
-
-    def __init__(self, expression: str):
-        self.expression = expression
-
-    def __str__(self):
-        return self.expression
