97e8fdd91548d5b69516df4fb00908ca5f11c0b1
Frank Sachsenheim <funkyfuture@riseup.net>
2024-09-04 21:04:40 +0200

    test_reduce_whitespace: Adds test that encodes TEI's recommendations

--- tests/test_reduce_whitespace.py
+++ tests/test_reduce_whitespace.py
@@ -1,4 +1,8 @@
-from delb import Document, new_tag_node
+from textwrap import dedent
+
+from delb import new_tag_node, Document, TagNode
+
+from tests.utils import assert_equal_trees
 
 # test_subclasses implicitly also tests collapsing and trimming
 
@@ -55,3 +59,122 @@ def test_space_preserve():
     assert root.first_child.full_text == "I Roy - Touting I Self"
     assert root.css_select("matrix")[0].full_text == "HB 243 A  Re"
     assert root.css_select("matrix")[1].full_text == "HB 243 B\tRe"
+
+
+def test_tei_recommendation_examples():
+    # it's not much unfortunately
+    # https://wiki.tei-c.org/index.php/XML_Whitespace
+    # the contained errors are fixed w/o annotations
+
+    def parse_sample(s: str) -> TagNode:
+        d = Document(s)
+        d.reduce_whitespace()
+        return d.root
+
+    # ## "Where XML Considers Whitespace to be Significant"
+
+    sample_1 = parse_sample('<title type="main"/>')
+    sample_2 = parse_sample('<title     type = "main" />')
+    assert_equal_trees(sample_1, sample_2)
+
+    sample_1 = parse_sample("<name>JoAnn</name>")
+    sample_2 = parse_sample("<name>Jo Ann</name>")
+    assert sample_1.full_text != sample_2.full_text
+
+    # ## "Normalize = Collapse + Trim"  # noqa: E800
+
+    reference_sample = parse_sample("<name>Jo Ann</name>")
+    assert parse_sample("<name>Jo    Ann</name>") == reference_sample
+    assert parse_sample("<name>Jo\n    Ann</name>") == reference_sample
+    assert parse_sample("<name> Jo Ann</name>") == reference_sample
+    assert parse_sample("<name>\n    Jo Ann</name>") == reference_sample
+    assert parse_sample("<name>\n    Jo   \n    Ann\n</name>") == reference_sample
+
+    # ## "Default Whitespace Processing"
+
+    sample = parse_sample(
+        dedent(
+            """\
+        <persName>
+          <forename>Edward</forename>
+          <forename>George</forename>
+          <surname type="linked">Bulwer-Lytton</surname>, <roleName>Baron Lytton of
+          <placeName>Knebworth</placeName>
+          </roleName>
+        </persName>"""
+        )
+    )
+    assert sample.full_text == "Edward George Bulwer-Lytton, Baron Lytton of Knebworth"
+    assert (
+        sample.full_text != " Edward George Bulwer-Lytton, Baron Lytton of Knebworth "
+    )
+    assert sample.full_text != "EdwardGeorgeBulwer-Lytton,BaronLyttonofKnebworth"
+
+    # #### "Text-Only Elements"
+
+    sample_1 = parse_sample("<country>Australia</country>")
+    sample_2 = parse_sample("<country>   Australia   </country>")
+    sample_3 = parse_sample("<country>\n        Australia     \n</country>")
+    assert sample_1.full_text == sample_2.full_text == sample_3.full_text
+
+    assert (
+        parse_sample(
+            "<emph xml:space='preserve' rend='underline'> Yes! </emph>"
+        ).full_text
+        == " Yes! "
+    )
+
+    sample_1 = parse_sample("<name>Ralph Waldo Emerson</name>")
+    sample_2 = parse_sample("<name>   Ralph Waldo  Emerson   </name>")
+    sample_3 = parse_sample(
+        dedent(
+            """\
+        <name>
+                Ralph    
+                Waldo    
+               Emerson   
+        </name>"""  # noqa: W291
+        )
+    )  # noqa: E124
+    assert sample_1.full_text == sample_2.full_text == sample_3.full_text
+
+    # #### "Mixed-Content Elements"
+
+    sample_1 = parse_sample(
+        dedent(
+            """\
+        <p>  The <emph> cat </emph> ate  the <foreign>grande croissant</foreign>. I didn't!
+          </p>"""  # noqa: E501
+        )
+    )
+    sample_2 = parse_sample(
+        """\
+        <p>The 
+        <emph>cat</emph>
+        ate the 
+        <foreign>grande croissant</foreign>. 
+        I didn't!</p>"""  # noqa: W291
+    )
+    sample_3 = parse_sample(
+        "<p>The <emph>cat</emph> ate the <foreign>grande croissant</foreign>. "
+        "I didn't!</p>"
+    )
+    assert sample_1.full_text == sample_2.full_text == sample_3.full_text
+
+    assert (
+        parse_sample(
+            "<p>The<emph> cat </emph>ate the <foreign>grande croissant</foreign>. "
+            "I didn't!</p>"
+        ).full_text
+        == "Thecatate the grande croissant. I didn't!"
+    )
+
+    # ## "Structured Elements and xsl:strip-space"  # noqa: 800
+
+    sample_1 = parse_sample(
+        "<root><settlement>New</settlement><settlement>York</settlement></root>"
+    )
+    sample_2 = parse_sample(
+        "<root><settlement>New</settlement> <settlement>York</settlement></root>"
+    )
+    assert sample_1.full_text != sample_2.full_text
