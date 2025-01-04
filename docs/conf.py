e53b405a02de69bb0c77a972512643650b3915f4
Frank Sachsenheim <funkyfuture@riseup.net>
2025-01-04 22:30:48 +0100

    Updates copyright notices to '25

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import re
 # -- Project information -----------------------------------------------------
 
 project = "delb"
-copyright = "2018-'24, Frank Sachsenheim"
+copyright = "2018-'25, Frank Sachsenheim"
 author = "Frank Sachsenheim"
 
 # The full version, including alpha/beta/rc tags
